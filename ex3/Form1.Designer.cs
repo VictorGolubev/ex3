4ede84711d22c17d587dcfd87664765054e00ed5
Your Name <you@example.com>
2021-01-23 11:02:26 +0300

    first commit

---
+++ ex3/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace ex3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 31);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Исходный матрица";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 251);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Сумма";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(22, 55);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(542, 191);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.WordWrap = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(30, 275);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(172, 26);
+            this.textBox2.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(211, 16);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 35);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Ввод массива";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(404, 266);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(142, 35);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Выполнить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(30, 350);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(626, 26);
+            this.textBox3.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 325);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(147, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Количество строк";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(840, 411);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ ex3/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace ex3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        ChildClass t = new ChildClass();
+        //размер матрицы
+        int n = 0;
+        int m = 0;
+        List<List<int>> matr = new List<List<int>>();
+        private void button1_Click(object sender, EventArgs e)
+        {           
+            string[] stroki = textBox1.Text.Split('\n');
+            foreach (string stroka in stroki)
+            {
+                matr.Add(new List<int>());
+                string[] q = stroka.Split(' ');
+                int temp_m = 0;
+                foreach (string temp in q)
+                {
+                    matr[n].Add(Convert.ToInt32(temp));
+                    temp_m++;
+                }
+                n++;
+                m = temp_m;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            t.getSum(matr);
+            textBox2.Text = t.sum.ToString();
+            int i = 1;
+            foreach(bool f in t.ODD)
+            {
+                textBox3.Text+= "Четность "+ i + "-й строки =" + f + "   \n";
+            }
+
+        }
+
+        class ParentClass
+        {
+            protected List<bool> oddOrNot = new List<bool>();
+            public List<bool> ODD
+            {
+                get
+                {
+                    return oddOrNot;
+                }
+
+                set
+                {
+                    oddOrNot = value;
+                }
+            }
+            public int sum;
+        }
+
+        class ChildClass : ParentClass
+        {
+            public bool getOdd(List<int> stroka)
+            {
+                foreach (int temp in stroka)
+                    if (temp % 2 == 0)
+                    {
+                        return true;
+                    }
+                return false;
+
+            }
+
+            public void getSum(List<List<int>> matr)
+            {
+                List<bool> result = new List<bool>();
+                int i = 1;
+                int sum = 0;
+                foreach (List<int> stroka in matr)
+                {
+                    result.Add(getOdd(stroka));
+                    if (getOdd(stroka))
+                        sum += i;
+                    i++;
+                }
+                this.sum = sum;
+                this.ODD = result;
+            }
+
+        }
+    }
+}
